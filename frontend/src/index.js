7f26796c410a5fa5cb7d9c069d51ae74e80ca136
adesh1616 <adeshsinkushwaha@gmail.com>
2024-04-11 16:41:13 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bulma/css/bulma.min.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import { UserProvider } from "./context/UserContext";
 
-ReactDOM.render(
-  <UserProvider>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <App />
-  </UserProvider>,
-  document.getElementById("root")
 );
